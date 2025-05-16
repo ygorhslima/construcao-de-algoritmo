fc7a86d6d35d868ae470670f28ba9e1828fa2242
ygorhslima <ygorlima.dev06@gmail.com>
2025-05-16 10:25:54 -0300

    arquivos de estudo de construção de algoritmo

---
+++ aula03.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int A,B,S;
+    scanf("%d",&A);
+    scanf("%d",&B);
+    S=A+B;    
+    printf("%d\n",S);
+    return 0;
+}
---
+++ aula04.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "olá mundo\n";
+    return 0;    
+}
