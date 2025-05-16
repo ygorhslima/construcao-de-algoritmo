0c7e03c525348e79b08e263239153c67797a4d6b
ygorhslima <ygorlima.dev06@gmail.com>
2025-05-16 11:02:12 -0300

    .

---
+++ aula05.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    long int numero = 100000000000000;
+    int numero_negativo = -10;
+    float numero2 = 10.5;
+    double numero3 = 10.333333;
+    char letra = 'a';
+    string nome = "joão";
+
+    cout << sizeof(numero) << endl;
+
+    return 0;    
+}
