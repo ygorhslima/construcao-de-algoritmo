0ed46f95550e96e39b47258635f59c6e8d56e5f4
ygorhslima <ygorlima.dev06@gmail.com>
2025-05-27 11:16:34 -0300

    entendendo bigO

--- aula25.py
+++ aula25.py
@@ -2,10 +2,14 @@ import os
 import math
 
 velocidade = 1
-tam_array = 10
+tam_array = 1000
 
 os.system("cls")
 print(f"tamanho array: {tam_array} elementos, velocidade: {velocidade} operação(ões) por segundo")
 
+print(f"O(log n): {round(math.log2(tam_array)/velocidade,1)} segundos")
 print(f"O(n): {tam_array} segundos")
+print(f"O(n log n): ")
+print(f"O(n²): ")
+print(f"O(n!): ")
 
