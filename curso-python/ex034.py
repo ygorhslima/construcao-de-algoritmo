b5b76506fdbdb6ef5b6744f6d5cdc6e5c889c80a
ygorhslima <ygorlima.dev06@gmail.com>
2025-06-03 18:28:05 -0300

    Update ex034.py

--- curso-python/ex034.py
+++ curso-python/ex034.py
@@ -1,3 +1,19 @@
-# escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento
+# escreva um programa que:
+
+#  pergunte o salário de um funcionário
+salario = float(input("digite o salário do funcionário: "))
+
+#  calcule o valor do seu aumento:
+
 #para salários superiores a R$1.250,00, calcule um aumento de 10%
+if salario > 1250.00:
+    aumento = salario * (10/100)
+    novo_salario = salario + aumento
+    print(f"o salário do funcionário é de {salario} e com aumento de 10% ficou {novo_salario}")
+
+
 # para os inferiores ou iguais, o aumento é de 15%
+if salario <= 1250:
+    aumento = salario * (15/100)
+    novo_salario = salario + aumento
+    print(f"o salário funcionário é de {salario} e com aumento de 15% ficou {novo_salario}")
