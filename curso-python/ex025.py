56b743dfc596f20d7c71b332b2646cf11bad253f
ygorhslima <ygorlima.dev06@gmail.com>
2025-06-03 09:37:39 -0300

    terminando exercicios de logica

--- curso-python/ex025.py
+++ curso-python/ex025.py
@@ -1,1 +1,3 @@
 # crie um programa que leia o nome de uma pessoa e diga se ela tem silva no nome
+n = input("digite seu nome: ")
+print(f"seu nome tem silva?: {'silva' in n}")
--- curso-python/ex026.py
+++ curso-python/ex026.py
@@ -2,3 +2,7 @@
 # quantas vezes aparece a letra A
 # em que posição ela aparece a primeira vez
 # em que posição ela aparece a última vez
+frase = input("digite uma frase qualquer: ")
+print(f"quantas vezes aparece a letra A?: {frase.count('a')}")
+print(f"em que posição o primeiro A apareceu?: {frase.find('a')} ")
+print(f"em que posição o A aparece a última vez?: {frase.rfind("a")}")
