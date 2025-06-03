f2a60ef9c3e6dc3be1fd84cbfb2fc063d40e6932
ygorhslima <ygorlima.dev06@gmail.com>
2025-06-03 16:59:39 -0300

    .

--- curso-python/ex012.py
+++ curso-python/ex012.py
@@ -1,4 +1,5 @@
 #leia o preço de um produto
 preco = float(input("qual o preço do produto?: "))
-novo_preco = preco * (5/100)
+desconto = preco * (5/100)
+novo_preco = preco - desconto
 print(f"o preço anterior é de R${preco} e o seu novo preço com 5% de desconto é de R${novo_preco}")
