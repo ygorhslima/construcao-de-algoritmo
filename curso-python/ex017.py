2433ad1704e182a501dbbf879c9c66546c999626
ygorhslima <ygorlima.dev06@gmail.com>
2025-06-05 10:15:37 -0300

    .

--- curso-python/ex017.py
+++
@@ -1,6 +0,0 @@
-import math 
-cateto_oposto = float(input("Comprimento do cateto oposto: "))
-cateto_adjacente = float(input("Comprimento do cateto adjacente: "))
-
-hipotenusa = math.hypot(cateto_oposto,cateto_adjacente)
-print(f"o calculo da hipotesuna foi de {hipotenusa}")
--- curso-python/teste/teste.py
+++
@@ -1,5 +0,0 @@
-s = 0
-for c in range(0,4):
-    n = int(input("digite um valor: "))
-    s += n
-print(f"o somatório dos valores foi {s}")
