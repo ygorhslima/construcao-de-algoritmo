f75782bd868b60d4a9661590baf4b78cb0c59356
ygorhslima <ygorlima.dev06@gmail.com>
2025-06-08 15:47:07 -0300

    fazendo algoritmo de opções de menu com while e fatorial

--- curso-python/ex060.py
+++ curso-python/ex060.py
@@ -2,3 +2,17 @@
 
 # ex:
 # 5! = 5x4x3x2x1 = 120
+
+n = int(input("digite um número para calcular seu fatorial:"))
+contador = n
+fat = 1
+
+print(f"{n}! = ", end="")
+
+while contador > 0:
+    print(f"{contador}", end="")
+    print(" x " if contador > 1 else " = ", end="")
+    fat *= contador
+    contador -= 1
+
+print(fat)
