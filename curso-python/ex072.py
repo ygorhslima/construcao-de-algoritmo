38add04b4905fd0006b750bb438e500f939f2d93
ygorhslima <ygorlima.dev06@gmail.com>
2025-06-10 12:00:47 -0300

    Update ex072.py

--- curso-python/ex072.py
+++ curso-python/ex072.py
@@ -8,16 +8,21 @@
 
 contagem = (
     "Zero","Um","Dois","Três","Quatro","Cinco","Seis","Sete","Oito",
-    "Nove","Dez","Onze","Doze","Treze","Catorze","Quinze","Dezesseis","Dezessete",
-    "Dezoito","Dezenove","Vinte"
+    "Nove","Dez","Onze","Doze","Treze","Catorze","Quinze","Dezesseis","Dezessete","Dezoito","Dezenove","Vinte"
 )
 
 while True:
     n = int(input("digite um número entre 0 e 20: "))
-
     if n >= 0 and n <= 20:
         print(f"você digitou o número {contagem[n]}")
-        break
+        
+        resp = ' '
+        while resp not in "SN":
+            resp = input("você quer continuar [S/N]: ").strip().upper()[0]
+        
+        if resp == 'N':
+            print("saindo...")
+            break
     else:
-        n = int(input("Tente novamente. Digite um número entre 0 e 20: "))
+        n = int(input("Digite um número entre 0 e 20: "))
     
