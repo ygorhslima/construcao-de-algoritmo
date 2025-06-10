724d3da5794aad7648e253c52697247b59c4c339
ygorhslima <ygorlima.dev06@gmail.com>
2025-06-10 10:57:17 -0300

    .

--- curso-python/ex022.py
+++
@@ -1,6 +0,0 @@
-nome = str(input("digite seu nome completo: ")).strip()
-
-print(f"Frase em maiúsucula {nome.upper()}")
-print(f"Frase em minúscula {nome.lower()}")
-print(f"quantas letras ao todo?: {len(nome.replace(' ',''))}")
-print(f"quantas letras tem o primeiro nome?: {len(nome.split()[0])}")
--- curso-python/ex024.py
+++
@@ -1,6 +0,0 @@
-# crie um programação que leia o nome de uma cidade e diga se ela começa ou não com o nome 'santo'
-
-cidade = str(input("digite um nome de uma cidade: ")).strip().lower()
-# verificando o nome da cidade, o split vai dividir a frase e vai pegar no índice 0 a primeira palavra, se for santo retorna True, senão, retorna False
-verificar_nome = cidade.split()[0] == "santo"
-print(f"A cidade começa com santo?: {verificar_nome}")
